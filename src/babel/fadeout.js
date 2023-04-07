ff1514e2b5bef8ed8bf2372eed8c076c4a85d370
M-minowa <minowa@impv.co.jp>
2023-04-07 16:31:29 +0900

    FBコメント分修正

--- src/babel/fadeout.js
+++ src/babel/fadeout.js
@@ -9,8 +9,12 @@ const priceListScroll = document.querySelector(".price-list__scroll");
 priceList.addEventListener("scroll", () => {
   // scrollLeftプロパティの値を確認して、条件を満たす場合には要素をフェードアウトする
   if (priceList.scrollLeft > 20) {
-    priceListScroll.style.display = "none";
+    priceListScroll.classList.add("is-hide");
   }
+
+  setTimeout(() => {
+    priceListScroll.style.display = "none";
+  }, "1000");
 });
 
 
