b2aef39d5fa10527f6478289fd7bd43d04ae3f5d
M-minowa <minowa@impv.co.jp>
2023-05-01 12:34:41 +0900

    FAQ以下作成

---
+++ src/babel/scroll.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// scroll
+// ---------------------------------------- //
+
+const toTop = document.getElementById("js-to-top");
+
+window.addEventListener("scroll", () => {
+  if (100 < window.scrollY) {
+    toTop.classList.add("is-active");
+  } else {
+    toTop.classList.remove("is-active");
+  }
+});
