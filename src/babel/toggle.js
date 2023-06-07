99e02040f6c72c48f5738c44e4fa8c0dad0b4026
M-minowa <minowa@impv.co.jp>
2023-06-07 10:22:39 +0900

    アコーディオンメニューの修正

--- src/babel/toggle.js
+++ src/babel/toggle.js
@@ -3,46 +3,33 @@
 // toggle
 // ---------------------------------------- //
 
-// メニューを開く関数
-const slideDown = (el) => {
-  el.style.height = 'auto'; //いったんautoに
-  let h = el.offsetHeight; //autoにした要素から高さを取得
-  el.style.height = h + 'px';
-  el.animate([ //高さ0から取得した高さまでのアニメーション
-    { height: 0 },
-    { height: h + 'px' }
-  ], {
-    duration: 300, //アニメーションの時間（ms）
-   });
-};
+///メニューが開いたら矢印ボタンを上にする
+// メニューの開閉
 
-// メニューを閉じる関数
-const slideUp = (el) => {
-  el.style.height = 0;
-};
+const accordions = document.querySelectorAll(".accordion__item");
 
-//アコーディオンコンテナ全てで実行
-const accordions = document.querySelectorAll('ul.include-accordion');
+console.log(accordions);
+
 accordions.forEach((accordion) => {
+  const answer = accordion.querySelector(".faq__answer");
 
-  //アコーディオンボタン全てで実行
-  const accordionBtns = accordion.querySelectorAll('.accordionBtn');
-  accordionBtns.forEach((accordionBtn) => {
-    accordionBtn.addEventListener('click', (e) => {
-      e.target.parentNode.classList.toggle('active'); //ボタンの親要素（=ul>li)にクラスを付与／削除
-      const content = accordionBtn.nextElementSibling; //ボタンの次の要素（=ul>ul）
-      if(e.target.parentNode.classList.contains('active')){
-        slideDown(content); //クラス名がactive（＝閉じていた）なら上記で定義した開く関数を実行
-      }else{
-        slideUp(content); //クラス名にactiveがない（＝開いていた）なら上記で定義した閉じる関数を実行
-      }
-      //スクロール制御のために上位階層ulのクラス名を変える
-      let container = accordion.closest('.scroll-control'); //sroll-controlnのクラス名である親要素を取得
-      if(accordionBtn.parentNode.classList.contains('active') == false && container !== null ){
-        container.classList.remove('active')
-      }else if (container !== null){
-        container.classList.add('active')
-      }
-    });
+  accordion.addEventListener("click", () => {
+    if (accordion.classList.contains("active")) {
+      //閉じてる時
+      accordion.classList.remove("active");
+      answer.style.height = 0;
+    } else {
+      //開いてる時
+      accordion.classList.add("active");
+      answer.style.height = "auto";
+      // answerの高さを取得する
+      let height = answer.offsetHeight;
+      // answerの高さをpxで表示する
+      answer.style.height = height + "px";
+      answer.animate([
+        { height: 0},
+        { height: height + "px" }
+      ], 300)
+    }
   });
-});
+})
